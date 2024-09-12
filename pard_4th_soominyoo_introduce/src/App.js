f32d2863f7656e60ccd6d3809c818fd80214e221
DooMin <tnals2832@naver.com>
2024-09-12 21:12:52 +0900

    add: 1st assignment

--- pard_4th_soominyoo_introduce/src/App.js
+++ pard_4th_soominyoo_introduce/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import photo from './sooming.JPG';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Profile">
+      <header className="Profile-header">
+        <div className='Photo'>
+          <img 
+            src={photo} 
+            className="Profile-photo" 
+            alt="My profile" 
+          />
+        </div>
+        <div className='Name'>
+          <h2>유수민</h2>
+          <h4>안녕하세요 수민이에오</h4>
+        </div>
+
+        <div className='MBTI'>
+          <h2>MBTI</h2>
+          <h4>ENTP</h4>
+        </div>
+        
+        <div className='Purpose'>
+          <h2>이번학기 목표</h2>
+          <h4>졸업 취업 복권당첨</h4>
+        </div>
+
+        <div className='Fav'>
+          <h2>좋아하는 것</h2>
+          <h4>누워있기 차 돈</h4>
+        </div>
+
       </header>
     </div>
   );
